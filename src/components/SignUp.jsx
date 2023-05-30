7f06f0aae4d593fdefeafeb5cea0c1ffc8b67ad3
KrzysztofJuszcze <krzysztof.juszcze@gmail.com>
2023-05-30 19:49:49 +0200

    Add styles

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,8 @@
 import {useNavigate, Link} from 'react-router-dom'
 import supabase from "../services/supabase";
+import Grid from '@mui/system/Unstable_Grid/Grid';
+import { TextField } from '@mui/material';
+import StandardButton from './StandardButton';
 
 function SignUp() {
 
@@ -30,17 +33,29 @@ if (!error) {
 
     return (
     <div>
+        <Grid item xl={12} sx={{display:'flex', justifyContent:'center'}}>
         <h1>Sign Up</h1>
+        </Grid>
+        <Grid container spacing={2} marginTop={2} sx={{display:"flex", flexDirection:"column", alignItems:"center", justifyContent:"center"}}>
         <form onSubmit={handleSignUp}>
-            <input id="email" placeholder="Email adress"/> 
-            <br/>
-            <input id="password" type="password" placeholder="Password"/> 
-            <br/>
-            <input id="password_confirm" type="password" placeholder="Confirm password"/>
-            <br/> 
-            <button>Sign Up</button>  
+        <Grid item xl={12}>
+            <TextField id="email" name="Email adress" label="Email adress"/> 
+        </Grid>
+        <Grid item xl={12}>
+            <TextField id="password" type="password" name="Password" label="Password"/> 
+        </Grid>
+        <Grid item xl={12}>
+            <TextField id="password_confirm" type="password" name="Confirm password" label="Confirm password"/> 
+        </Grid>
+        <Grid item xl={12} sx={{display:'flex', justifyContent:'center'}}>
+            <StandardButton text='Sign Up'sx={{ width: '100%' }}/>
+        </Grid>
         </form>
+        </Grid>
+        <Grid item xl={12} sx={{display:'flex', justifyContent:'center', marginTop:'30px'}}>
+        <a style={{marginRight: '10px'}}>Already signed up?</a>
         <Link to='/signin'>SignIn</Link>
+        </Grid>
     </div>
     )
 }
