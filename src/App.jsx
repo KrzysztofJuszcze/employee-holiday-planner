ed919372c4d8602a8c5424ad559f981a264c225e
KrzysztofJuszcze <krzysztof.juszcze@gmail.com>
2023-06-10 10:48:10 +0200

    Corrected details

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import './App.css'
+import './main.scss'
 import Main from './components/Main';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import Staff from './components/Staff';
+import AddEmployer from './components/AddEmployer';
+import DisplayEmployeeList from './components/DisplayEmployeeList';
+import Planner from './components/Planner';
+import HolidayEntitlement from './components/HolidayEntitlement';
+import CalendarHoliday from './components/CalendarHoliday';
+
 
 function App() {
 return (
@@ -11,6 +18,11 @@ return (
       <Route path="/" element={<Main />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/staff" element={<Staff />} />
+      <Route path="/addemployer" element={<AddEmployer />}  />
+      <Route path="/displayemployeelist" element={<DisplayEmployeeList />}  />
+      <Route path="/holidayentitlement" element={<HolidayEntitlement />}  />
+      <Route path="/planner" element={<CalendarHoliday />}  />
     </Routes>
   </Router>
 )
